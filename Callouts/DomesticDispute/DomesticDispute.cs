b18b8f1e586061910b93f81a53cdd375bfa93bcc
NorthCam18 <55010072+NorthCam18@users.noreply.github.com>
2020-04-11 15:52:33 -0600

    Callout updates

---
+++ Callouts/DomesticDispute/DomesticDispute.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CalloutAPI;
+
+namespace DomesticDispute
+{
+    [CalloutProperties("DomesticDispute", "NorthCam18", "1.0", Probability.Medium)]
+    public class DomesticDispute : CalloutAPI.Callout
+    {
+        Ped suspect, victim;
+        public DomesticDispute()
+        {
+            Random random = new Random();
+            float offsetX = random.Next(100, 800);
+            float offsetY = random.Next(100, 800);
+
+            InitBase(World.GetNextPositionOnSidewalk(Game.PlayerPed.GetOffsetPosition(new Vector3(offsetX, offsetY, 0))));
+
+            this.ShortName = "Domestic Dispute";
+            this.CalloutDescription = "Neighbours have reported loud screaming and fighting at the residence! Things are heating up, get there quick!";
+            this.ResponseCode = 3;
+            this.StartDistance = 30f;
+        }
+
+        public async override Task Init()
+        {
+            OnAccept();
+
+            suspect = await SpawnPed(GetRandomPed(), Location);
+            victim = await SpawnPed(GetRandomPed(), Location);
+
+            suspect.AlwaysKeepTask = true;
+            suspect.BlockPermanentEvents = true;
+            victim.AlwaysKeepTask = true;
+            victim.BlockPermanentEvents = true;
+        }
+
+        public override void OnStart(Ped player)
+        {
+            base.OnStart(player);
+
+            if (Game.Player.Character.Position.DistanceToSquared(Location) <= 10f)
+            {
+                Notify("~r~[SUSPECT] Well you never cleaned up the house when I told you to!");
+                Wait(5000);
+                Notify("~b~[VICTIM] Well you never let me go out with my friends! So I'm sorry, but I didn't think it was fair!");
+                Wait(5000);
+                Notify("~r~[SUSPECT] Oh my GOD! I'm tired of your shit!");
+                Wait(1000);
+
+                suspect.Task.FightAgainst(victim);
+                victim.Task.ReactAndFlee(suspect);
+            }
+        }
+
+        private void Notify(string message)
+        {
+            SetNotificationTextEntry("STRING");
+            AddTextComponentString(message);
+            DrawNotification(false, false);
+        }
+    }
+}
---
+++ Callouts/ParkingViolation/ParkingViolation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CalloutAPI;
+
+namespace ParkingViolation
+{
+    [CalloutProperties("ParkingViolation", "NorthCam18", "1.0", Probability.Low)]
+    public class ParkingViolation : CalloutAPI.Callout
+    {
+        Vehicle vehicle;
+        private string[] modelsList = new string[] { "akuma", "faggio", "blista", "issi", "f620", "jackal", "asea", "fugitive", "primo", "feltzer", "casco", "infernus", "buccaneer", "dukes", "baller", "beejayxl", "bison", "boxville", "burrito", "rumpo" };
+        public ParkingViolation()
+        {
+            Random random = new Random();
+            float offsetX = random.Next(100, 1000);
+            float offsetY = random.Next(100, 1000);
+
+            InitBase(World.GetNextPositionOnSidewalk(Game.PlayerPed.GetOffsetPosition(new Vector3(offsetX, offsetY, 0))));
+
+            this.ShortName = "Parking Violation";
+            this.CalloutDescription = "Caller reported a vehicle parked illegally, respond and take care of it!";
+            this.ResponseCode = 1;
+            this.StartDistance = 80f;
+        }
+
+        public async override Task Init()
+        {
+            OnAccept(15, BlipColor.Blue, BlipSprite.PersonalVehicleCar, 100);
+            
+            Random random = new Random();
+            var selectedModel = modelsList[random.Next(modelsList.Length)];
+
+            var selectedHash = (VehicleHash) GetHashKey(selectedModel);
+
+            vehicle = await SpawnVehicle(selectedHash, Location);
+
+            vehicle.IsPersistent = true;
+            vehicle.IsEngineRunning = false;
+        }
+
+        public override void OnStart(Ped player)
+        {
+            base.OnStart(player);
+
+            if (Game.Player.Character.Position.DistanceToSquared(Location) <= 10f)
+            {
+                DisplayHelpTextThisFrame("~b~Write the vehicle a citation, then tow it away!", false);
+                Wait(5000);
+            }
+        }
+    }
+}
