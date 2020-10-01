ceeb090c0015e7aa361abe2a43ee24c4773e37ea
KDani-99 <porsekin@gmail.com>
2020-10-01 17:14:43 +0200

    feat: EntityExtensions

---
+++ Source/Utils/EntityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CitizenFX.Core;
+using System.Threading.Tasks;
+
+namespace FivePD.API.Utils
+{
+    static class EntityExtensions
+    {
+        #region Blip
+        public static void Sync(this Blip blip) => Utilities.SyncBlip(blip);
+        public static void Sync(this Blip blip,float radius) => Utilities.SyncBlipWithRadius(blip,radius);
+        public static void SyncDelete(this Blip blip) => Utilities.SyncBlipDelete(blip);
+        #endregion
+
+        #region Ped
+        public static Task<PedData> GetData(this Ped ped) => Utilities.GetPedData(ped.NetworkId);
+        public static void SetData(this Ped ped, PedData data) => Utilities.SetPedData(ped.NetworkId, data);
+        public static Ped GetClosest(this Ped ped) => Utilities.GetClosestPed(ped);
+        #endregion
+
+        #region Vehicle
+        public static Task<VehicleData> GetData(this Vehicle vehicle) => Utilities.GetVehicleData(vehicle.NetworkId);
+        public static void SetData(this Vehicle vehicle, VehicleData data) => Utilities.SetVehicleData(vehicle.NetworkId, data);
+        #endregion
+    }
+}
