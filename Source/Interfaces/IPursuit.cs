d2b2f00a42805f874b1040c4b60353d39d267617
KDani-99 <porsekin@gmail.com>
2020-10-01 17:17:30 +0200

    feat: IPursuit interface

---
+++ Source/Interfaces/IPursuit.cs
@@ -0,0 +1,27 @@
+﻿using CitizenFX.Core;
+
+namespace FivePD.API
+{
+    public enum PursuitStateEnum
+    {
+        Awaiting,
+        Active,
+        Finished
+    }
+    public interface IPursuit<PursuitStateEnum>
+    {
+        int Handle { get; }
+
+        bool IsInPursuit { get; }
+        bool IsVehiclePursuit { get; }
+
+        Ped Suspect { get; }
+        Ped FleeingFrom { get; }
+
+        PursuitStateEnum PursuitState { get; }
+
+        void Init(bool vehiclePursuit, float fleeDistance, float areaRadius, bool suspectBlip = true);
+        void ActivatePursuit();
+        void Terminate();
+    }
+}
