cdb9a667940547634c4c3dca09431adc767f1661
KDani-99 <porsekin@gmail.com>
2020-10-01 17:18:32 +0200

    feat: Pursuit handler class

---
+++ Source/Pursuit.cs
@@ -0,0 +1,17 @@
+﻿using CitizenFX.Core;
+
+namespace FivePD.API
+{
+    public abstract class Pursuit
+    {
+        public delegate IPursuit<PursuitStateEnum> RegisterPursuitDelegate(Ped suspect);
+        public static RegisterPursuitDelegate RegisterPursuit;
+
+        public delegate IPursuit<PursuitStateEnum>[] GetPursuitsByStateDelegate(PursuitStateEnum pursuitState);
+        public static GetPursuitsByStateDelegate GetPursuitsByState;
+
+        public delegate IPursuit<PursuitStateEnum> GetPursuitByHandleDelegate(int handle);
+        public static GetPursuitByHandleDelegate GetPursuitByHandle;
+    }
+    
+}
