79d0136dc3b31b9b557f91bd494cf5d6f682f7c0
KDani-99 <porsekin@gmail.com>
2021-03-27 14:59:16 +0100

    Move CalloutPropertiesAttribute from Callout to it's own file

---
+++ Source/Attributes/CalloutPropertiesAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FivePD.API
+{
+    /// <summary>
+    /// Bundles callout properties (CalloutPropertiesAttribute is used in the CalloutManager)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public sealed class CalloutPropertiesAttribute : Attribute
+    {
+        /// <summary>The name of the callout as shown in the Debug menu and in the console (See <see cref="Callout.ShortName"/> for the dispatch name)</summary>
+        public string name { get; private set; }
+        public string author { get; private set; }
+
+        /// <summary>
+        /// The version of the callout.
+        /// <example>
+        /// For example: <c>1.2.3</c>
+        /// </example>
+        /// </summary>
+        public string version { get; private set; }
+
+        /// <summary>
+        /// Callout properties
+        /// </summary>
+        /// <param name="name">The name of the callout as shown in the Debug menu and in the console (See <see cref="Callout.ShortName"/> for the dispatch name)</param>
+        /// <param name="version">The version of the callout. <example>For example: <c>1.2.3</c></example></param>
+        public CalloutPropertiesAttribute(string name, string author, string version)
+        {
+            this.name = name;
+            this.version = version;
+            this.author = author;
+        }
+    }
+}
--- Source/Callout.cs
+++ Source/Callout.cs
@@ -69,7 +69,7 @@ namespace FivePD.API
         }
 
         public float Radius;
-        public Callout() { }
+        public Callout() {  }
 
         /* Gameplay related methods */
 
@@ -256,35 +256,5 @@ namespace FivePD.API
         protected event Func<Task> Tick;
 
         #endregion
-    }
-
-    /// <summary>
-    /// Bundles callout properties (CalloutPropertiesAttribute is used in the CalloutManager)
-    /// </summary>
-    public class CalloutPropertiesAttribute : Attribute
-    {
-        /// <summary>The name of the callout as shown in the Debug menu and in the console (See <see cref="Callout.ShortName"/> for the dispatch name)</summary>
-        public string name { get; private set; }
-        public string author { get; private set; }
-
-        /// <summary>
-        /// The version of the callout.
-        /// <example>
-        /// For example: <c>1.2.3</c>
-        /// </example>
-        /// </summary>
-        public string version { get; private set; }
-
-        /// <summary>
-        /// Callout properties
-        /// </summary>
-        /// <param name="name">The name of the callout as shown in the Debug menu and in the console (See <see cref="Callout.ShortName"/> for the dispatch name)</param>
-        /// <param name="version">The version of the callout. <example>For example: <c>1.2.3</c></example></param>
-        public CalloutPropertiesAttribute(string name, string author, string version)
-        {
-            this.name = name;
-            this.version = version;
-            this.author = author;
-        }
     }
 }
